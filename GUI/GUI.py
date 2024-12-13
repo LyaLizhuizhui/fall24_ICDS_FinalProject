806408998861ad558447575f32c6602c6c94d852
LyaLizhuizhui <LyaLizhuizhui@users.noreply.github.com>
2024-12-13 09:41:31 +0800

    Update GUI.py

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -648,64 +648,64 @@ class GUI:
         self.mat, self.flag = logic.move_up(self.mat)
         self.assign_mat()
         # get the current state and print it
-        self.g_status = logic.get_current_state(self.mat)
-        print(self.g_status)
+        # Get the game status, including invalid moves
+        status = logic.get_current_state(self.mat, flag)
 
-        # if game not over then continue
-        # and add a new two
-        if self.flag:
-            self.calc_total(self.g_status,self.total_score)
-            if(self.g_status == 'GAME NOT OVER'):
-                logic.add_new_2(self.mat)
-
-        # else break the loop 
-            else:
-                self.game_over()
-        #else
-        else:
-            print("Invalid move, no tiles moved. Try again.")
+        # Handle the different statuses
+        if status == 'INVALID MOVE':
+            print(status)
+        elif status == 'WE WON':
+            print("Congratulations, you WON!")
+            break
+        elif status == 'GAME OVER :(':
+            print("Game Over. You LOST!")
+            break
+        elif status == 'GAME NOT OVER':
+            logic.add_new_2(self.mat)
 
     def down(self, flag):
         self.mat, self.flag = logic.move_down(self.mat)
         self.assign_mat()
-        self.g_status = logic.get_current_state(self.mat)
-        print(self.g_status)
-        if self.flag:
-            self.calc_total(self.g_status,self.total_score)
-            if(self.g_status == 'GAME NOT OVER'):
-                logic.add_new_2(self.mat)
-            else:
-                self.game_over()
-        else:
-            print("Invalid move, no tiles moved. Try again.")
+        status = logic.get_current_state(self.mat, flag)
+        if status == 'INVALID MOVE':
+            print(status)
+        elif status == 'WE WON':
+            print("Congratulations, you WON!")
+            break
+        elif status == 'GAME OVER :(':
+            print("Game Over. You LOST!")
+            break
+        elif status == 'GAME NOT OVER':
+            logic.add_new_2(self.mat)
 
     def left(self, flag):
         self.mat, self.flag = logic.move_left(self.mat)
         self.assign_mat()
-        self.g_status = logic.get_current_state(self.mat)
-        print(self.g_status)
-        if self.flag:
-            self.calc_total(self.g_status,self.total_score)
-            if(self.g_status == 'GAME NOT OVER'):
-                logic.add_new_2(self.mat)
-            else:
-                self.game_over()
-        else:
-            print("Invalid move, no tiles moved. Try again.")
+        status = logic.get_current_state(self.mat, flag)
+        if status == 'INVALID MOVE':
+            print(status)
+        elif status == 'WE WON':
+            print("Congratulations, you WON!")
+            break
+        elif status == 'GAME OVER :(':
+            print("Game Over. You LOST!")
+            break
+        elif status == 'GAME NOT OVER':
+            logic.add_new_2(self.mat)
 
     def right(self, flag):
         self.mat, self.flag = logic.move_right(self.mat)
-        self.assign_mat()
-        self.g_status = logic.get_current_state(self.mat)
-        print(self.g_status)
-        if self.flag:
-            self.calc_total(self.g_status,self.total_score)
-            if(self.g_status == 'GAME NOT OVER'):
-                logic.add_new_2(self.mat)
-            else:
-                self.game_over()
-        else:
-            print("Invalid move, no tiles moved. Try again.")
+        status = logic.get_current_state(self.mat, flag)
+        if status == 'INVALID MOVE':
+            print(status)
+        elif status == 'WE WON':
+            print("Congratulations, you WON!")
+            break
+        elif status == 'GAME OVER :(':
+            print("Game Over. You LOST!")
+            break
+        elif status == 'GAME NOT OVER':
+            logic.add_new_2(self.mat)
 
         ##################### 2048 game window end ###################
     def draw_grid(self):
